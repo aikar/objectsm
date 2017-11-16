9104a4779f480deabd1aa754194add51395581d0
Aikar <aikar@aikar.co>
2017-11-15 23:33:40 -0500

    More work on this

--- src/jsonobject.js
+++ src/jsonobject.js
@@ -1,3 +1,5 @@
+/** @flow */
+/*
 /*
  * Copyright (c) (2017)
  *
@@ -7,27 +9,66 @@
  *
  */
 
-import {ArrayListMultiMap} from "./ArrayListMultiMap";
+export type Config = {
+  mappings: {[key: string]: Class<any>},
+  creators?: {[key: string]: ObjectCreator<any>},
+  logger?: Function,
+  typeKey?: string,
+}
+
+export class ObjectCreator {
+  objCls: Class<T>;
+
+  constructor(objCls: Class<any>) {
+    this.objCls = objCls;
+  }
+
+  createObject(data: DataParameter): any {
+    const tpl = Object.create(this.objCls.prototype);
+
+    for (const [key, val] of Object.entries(data)) {
+      tpl[key] = val;
+    }
+    return tpl;
+  }
+}
+const MapObjectCreator = new (class extends ObjectCreator {
+  createObject(data: DataParameter): any {
+    return new Map(Object.entries(data));
+  }
+})();
+const SetObjectCreator = new (class extends ObjectCreator {
+  createObject(data: DataParameter): any {
+    return new Set(data);
+  }
+})();
 
 export class JsonObject {
 
-  deseriaizedCount = 0;
-  typeKey;
-  id2ObjMap = new Map();
-  obj2IdMap = new Map();
-  logger = console.error.bind(console, "[JsonObject]");
-  postProcessors = new ArrayListMultiMap();
+  deseriaizedCount: number = 0;
+  typeKey: string;
+  id2ObjMap: Map<string, Class<any>> = new Map();
+  obj2IdMap: Map<Class<any>, string> = new Map();
+  logger: Function = console.error.bind(console, "[JsonObject]");
+  objCreators: Map<Class<any>, ObjectCreator<any>> = new Map();
 
-  constructor(config) {
+
+  constructor(config: Config) {
     const mappings = {
       "__MAP": Map,
       "__SET": Set,
       ...(config.mappings || {})
     };
+    const creators = config.creators || {};
+    this.objCreators.set("__MAP", MapObjectCreator);
+    this.objCreators.set("__SET", SetObjectCreator);
+
     for (const [id, obj] of Object.entries(mappings)) {
       this.id2ObjMap.set(id, obj);
       this.obj2IdMap.set(obj, id);
+      this.objCreators.set(id, creators[id] || obj.ObjectCreator || new ObjectCreator(obj));
     }
+
     if (config.logger) {
       this.logger = config.logger;
     }
@@ -50,19 +91,17 @@ export class JsonObject {
    * @param data
    * @returns {*}
    */
-  async deserializeObject(data) {
+  async deserializeObject(data: DataParameter) {
     const queue = [];
     if (Array.isArray(data)) {
       for (let i = 0; i < data.length; i++) {
         const arrData = data[i];
         data[i] = this.createObject(arrData);
-        this.initializeData(data[i], arrData, queue);
       }
       await this.processQueue(queue);
       return data;
     } else {
       const obj = this.createObject(data);
-      this.initializeData(obj, data, queue);
       await this.processQueue(queue);
       return obj;
     }
@@ -73,7 +112,7 @@ export class JsonObject {
    * @param {QueuedDeserialize[]} queue
    * @returns {Promise.<void>}
    */
-  async processQueue(queue) {
+  async processQueue(queue: Array<QueuedDeserialize>) {
     let item;
     while ((item = queue.pop())) {
       if (typeof item === 'function') {
@@ -87,7 +126,6 @@ export class JsonObject {
       const thisData = item.val[thisIdx];
       if (thisData[this.typeKey]) {
         item.val[thisIdx] = this.createObject(thisData);
-        this.initializeData(item.val[thisIdx], thisData, queue);
       } else if (typeof item.val[thisIdx] === 'object') {
         queueDeserialize(item.val[thisIdx], queue);
       }
@@ -103,7 +141,7 @@ export class JsonObject {
    * @param {JsonObjectBase} tpl
    * @returns {JsonObjectBase}
    */
-  async _deserializeObject(tpl) {
+  async _deserializeObject(tpl: IJsonOBjectBase) {
     const queue = [];
     if (tpl.onDeserialize != null) {
       queue.push(() => tpl.onDeserialize());
@@ -125,57 +163,45 @@ export class JsonObject {
   }
 
   /**
-   * @param {JsonObjectBase} tpl
-   * @param {object} data
-   * @param {QueuedDeserialize[]} queue
-   */
-  initializeData(tpl, data, queue) {
-    // eslint-disable-next-line private-props/no-use-outside
-    const deferDeserializing = tpl._deferDeserializing;
-
-    delete tpl['_deferDeserializing'];
-    delete tpl['deserializeObject'];
-    delete tpl['rawData'];
-
-    Object.defineProperty(tpl, 'rawData', {
-      enumerable: false,
-      configurable: true,
-      value: () => data
-    });
-
-    for (const [key, val] of Object.entries(data)) {
-      tpl[key] = val;
-    }
-
-    Object.defineProperty(tpl, 'deserializeObject', {
-      enumerable: false,
-      configurable: true,
-      value: () => this._deserializeObject(tpl)
-    });
-
-    if (!deferDeserializing) {
-      tpl.deserializeObject().catch(e => this.logger("Error deserializing object", e));
-    }
-  }
-
-  /**
    * @param data
    * @returns {JsonObjectBase}
    */
-  createObject(data) {
+  createObject(data: DataParameter) {
     const id = data[this.typeKey];
     const objCls = this.id2ObjMap.get(id);
+    const creator = this.objCreators.get(id);
 
     if (typeof id !== 'undefined' && typeof objCls === 'function') {
-      /**
-       * @type JsonObjectBase
-       *
-       */
-      const tpl = new objCls();
-      // How can we do this?
-      //const tpl = Object.create(null, objCls);
-      delete data[this.typeKey];
+      if (!creator || !creator.createObject) {
+        this.logger("Invalid Object Creator for", id,  objCls);
+        throw new Error("Invalid Object Creator for " + id);
+      }
 
+      const tpl = creator.createObject(data);
+
+      // eslint-disable-next-line private-props/no-use-outside
+      const deferDeserializing = tpl._deferDeserializing;
+
+      delete data[this.typeKey];
+      delete tpl['_deferDeserializing'];
+      delete tpl['deserializeObject'];
+      delete tpl['rawData'];
+
+      Object.defineProperty(tpl, 'rawData', {
+        enumerable: false,
+        configurable: true,
+        value: () => data
+      });
+
+      Object.defineProperty(tpl, 'deserializeObject', {
+        enumerable: false,
+        configurable: true,
+        value: () => this._deserializeObject(tpl)
+      });
+
+      if (!deferDeserializing) {
+        tpl.deserializeObject().catch(e => this.logger("Error deserializing object", e));
+      }
       return tpl;
     }
     this.logger("Unknown Class Data:", id, data);
@@ -185,27 +211,29 @@ export class JsonObject {
 
 export default JsonObject;
 
-export class JsonObjectBase {
-  _rawData;
+export interface IJsonOBjectBase {
+  _deferDeserializing?: boolean;
+  deserializeObject(): Promise<any>;
+  rawData(): any;
+  onDeserialize(): any;
+}
 
-  async deserializeObject() { /* abstract - will be injected from JsonObject */
-  }
+export class JsonObjectBase implements IJsonOBjectBase {
 
-  rawData() { /* abstract - will be injected from JsonObject */
-  }
+  /* abstract - will be injected from JsonObject */
+  async deserializeObject() {}
 
-  onDeserialize() {
-  }
+  /* abstract - will be injected from JsonObject */
+  rawData() {}
 
-  constructor() {
-  }
+  onDeserialize() {}
 }
 
 /**
  * @param {object,object[]} obj
  * @param {QueuedDeserialize[]} queue
  */
-function queueDeserialize(obj, queue) {
+function queueDeserialize(obj: DataParameter, queue: Array<QueuedDeserialize>) {
   return new Promise((a) => {
     if (Array.isArray(obj)) {
       for (let i = 0; i < obj.length; i++) {
@@ -224,10 +252,9 @@ function queueDeserialize(obj, queue) {
   });
 
 }
+export type DataParameter = {[key: string]: any} | Array<any>;
+type QueuedDeserialize = Function | {idx: any, val: any};
 
-/**
- * @typedef {function,{idx: *, val: *}} QueuedDeserialize
- */
 function waitFor(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
